fb6610521465ae5cb9117309cd0e39a59c43cb43
Eörs Szathmáry <eorsjr@gmail.com>
2025-08-10 10:08:38 +0200

    feat(navigation): :sparkles: centralize navigation item management

--- src/app/navigation/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation/navigation-bar/navigation-bar.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ScrollService } from '../../services/scroll.service';
+import { CommonModule } from '@angular/common';
+import { NavigationService } from '../../services/navigation.service';
 
 @Component({
   selector: 'cxw-navigation-bar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.css'
 })
 export class NavigationBarComponent {
-  constructor(public scrollService: ScrollService) {}
+  constructor(public navService: NavigationService, public scrollService: ScrollService) {}
 }
--- src/app/services/navigation.service.ts
+++ src/app/services/navigation.service.ts
@@ -2,12 +2,34 @@ import { Injectable } from '@angular/core';
 import { signal } from '@angular/core';
 import { ScrimService } from './scrim.service';
 
+/**
+ * Interface representing a navigation item.
+ */
+export interface NavItem {
+  routerLink?: string;
+  icon: string;
+  label: string;
+  ariaLabel: string;
+  showOnBar: boolean;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class NavigationService {
 
   constructor(private scrimService: ScrimService) { }
+
+  // Navigation items
+  public navItems: NavItem[] = [
+    { routerLink: '/', icon: 'home', label: 'Home', ariaLabel: 'Home', showOnBar: true },
+    { routerLink: '/components', icon: 'dashboard', label: 'Components', ariaLabel: 'Components', showOnBar: true },
+    { routerLink: '/extras', icon: 'extension', label: 'Extras', ariaLabel: 'Extras', showOnBar: true },
+    { routerLink: '/visuals', icon: 'photo', label: 'Visuals', ariaLabel: 'Visuals', showOnBar: true },
+    { routerLink: '/contact', icon: 'contact_page', label: 'Contact', ariaLabel: 'Contact', showOnBar: true },
+    {icon: 'stars', label: 'Label', ariaLabel: 'Label', showOnBar: false },
+    {icon: 'stars', label: 'Label', ariaLabel: 'Label', showOnBar: false }
+  ];
 
   public navigationRailOpen = signal(false); // Signal to track whether the navigation rail is open
 
